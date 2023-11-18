77c84273eeaae480a88b3b8db530770ff5548e90
erlams <elmaddin@gmail.com>
2023-11-18 19:22:11 +0100

    gh

---
+++ script.js
@@ -0,0 +1,10 @@
+let certti =  prompt ('YOU CAN GET THE CERTIFICATE');
+
+let netice;
+if (certti == 'game certificat ' ) {
+netice = 'YOU CAN GET THE CERtFICAT';
+
+}else {
+    netice = 'YOU CAN NOT TAKE IT';
+}
+document.getElementById('cer').innerHTML = netice;
